1a912dc268e96374201af953171b8dd2941f3054
Siddhesh-Mavrick <139612617+Siddhesh-Mavrick@users.noreply.github.com>
2024-05-03 01:46:21 +0530

    Update pattern.cpp

--- pattern.cpp
+++ pattern.cpp
@@ -6,8 +6,13 @@ int main() {
   cin >> n;
 
   for(int i = 0; i < n; i++) {
-      for(int j = 0; j < n ; j++) {
-          cout << " * ";
+      
+      for(int k = 0; k < n - 1 - i; k++) {
+          cout << "  ";
+      }
+      
+      for(int j = 0; j < 2 * i + 1 ; j++) {
+          cout << "* ";
       }
       cout << endl;
   }
